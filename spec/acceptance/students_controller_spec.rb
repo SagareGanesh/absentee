2ca41ac82271b841cc4a2e89ce262114607d600e
Ganesh Sagare <ganesh@joshsoftware.com>
2019-04-27 02:14:47 +0530

    added X-Api-Key for api spec

--- spec/acceptance/students_controller_spec.rb
+++ spec/acceptance/students_controller_spec.rb
@@ -4,6 +4,7 @@ require 'rspec_api_documentation/dsl'
 resource "V1" do
   header "Accept", "application/vnd.parenotify.com; version=1"
   header "Content-Type", "application/json"
+  header "X-Api-Key", "94b2fff3e7fd9b9c391a2306"
 
   post "/students/upload" do
     parameter :file, "New file to upload"
